a159f6d0048aaca3fc406a2495aebea1823d4358
jordão moura <jordaoantoniodev@gmail.com>
2023-03-03 22:52:30 -0300

    Meu Primeiro Projeto em Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+const routes: Routes = [
+{ path:'inicio', component: InicioComponent},
+{ path:'Login', component: LoginComponent},
+{path:'',redirectTo:'inicio',pathMatch:'full'}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
